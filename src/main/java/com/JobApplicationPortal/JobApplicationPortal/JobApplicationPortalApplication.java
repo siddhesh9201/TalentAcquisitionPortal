197649b7aa2cebfcd51f6e54432dbb33fb7425f5
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-24 15:24:07 +0530

    Initial commit

---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/JobApplicationPortalApplication.java
@@ -0,0 +1,13 @@
+package com.JobApplicationPortal.JobApplicationPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JobApplicationPortal/JobApplicationPortal/JobApplicationPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobApplicationPortal.JobApplicationPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
