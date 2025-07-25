711f53028ac91471dff2770de2c9c673f6ceb2b2
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-25 18:05:51 +0530

    implemented PostMapping for Registeration

--- src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/ClientController.java
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/ClientController.java
@@ -1,8 +1,25 @@
 package com.JobApplicationPortal.JobApplicationPortal.Controller;
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.ClientMapper.ClientIncomingDto;
+import com.JobApplicationPortal.JobApplicationPortal.Services.ClientServices;
+import org.hibernate.query.sql.spi.ParameterOccurrence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class ClientController {
+
+
+    @Autowired
+    ClientServices clientService;
+
+    @PostMapping("/Client/Register")
+    public ResponseEntity<String> registerClient(ClientIncomingDto client){
+            clientService.addClient(client);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Registration Successfully!");
+    }
 
 }
