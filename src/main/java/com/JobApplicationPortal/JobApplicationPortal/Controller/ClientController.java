e62d601697eb75af4be9426e15f8a9981f9d572e
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-25 22:13:08 +0530

    get request of seeker handle by controller that implemented

--- src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/ClientController.java
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/ClientController.java
@@ -1,12 +1,16 @@
 package com.JobApplicationPortal.JobApplicationPortal.Controller;
 import com.JobApplicationPortal.JobApplicationPortal.Mapper.ClientMapper.ClientIncomingDto;
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.ClientMapper.SeekerProfileOutgoingDto;
 import com.JobApplicationPortal.JobApplicationPortal.Services.ClientServices;
+import jakarta.validation.Valid;
 import org.hibernate.query.sql.spi.ParameterOccurrence;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -17,9 +21,14 @@ public class ClientController {
     ClientServices clientService;
 
     @PostMapping("/Client/Register")
-    public ResponseEntity<String> registerClient(ClientIncomingDto client){
+    public ResponseEntity<String> registerClient(@RequestBody @Valid ClientIncomingDto client){
             clientService.addClient(client);
         return ResponseEntity.status(HttpStatus.CREATED).body("Registration Successfully!");
+    }
+
+    @GetMapping("/seeker/{id}")
+    public ResponseEntity<SeekerProfileOutgoingDto>getSeekerProfile(@PathVariable  Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getSeeker(id));
     }
 
 }
