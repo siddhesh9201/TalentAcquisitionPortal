633cd3a30b271059919541d161b564e73d8767d4
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-25 17:59:05 +0530

    created ClientRepo Interface for JPA operation that interface extends JPA Repository

---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Repository/ClientRepo.java
@@ -0,0 +1,13 @@
+package com.JobApplicationPortal.JobApplicationPortal.Repository;
+import com.JobApplicationPortal.JobApplicationPortal.Model.Client;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepo extends JpaRepository<Client,Long> {
+    Optional<Client> findByEmail(@Email @NotNull String email);
+}
