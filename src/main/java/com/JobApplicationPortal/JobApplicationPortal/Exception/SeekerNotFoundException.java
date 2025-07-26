dc7c07d062c5a7b28976c03d536fb9b99ea95997
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-26 12:05:24 +0530

    Seeker not found exception class created

---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Exception/SeekerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.JobApplicationPortal.JobApplicationPortal.Exception;
+
+public class SeekerNotFoundException extends RuntimeException {
+    public SeekerNotFoundException(String message) {
+        super(message);
+    }
+}
