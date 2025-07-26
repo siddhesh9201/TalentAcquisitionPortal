e824d6d2d98fc0edaeeff747268cbdd9b63c893b
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-26 15:37:32 +0530

    Created

---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Exception/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.JobApplicationPortal.JobApplicationPortal.Exception;
+
+public class EmailNotFoundException extends RuntimeException {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
