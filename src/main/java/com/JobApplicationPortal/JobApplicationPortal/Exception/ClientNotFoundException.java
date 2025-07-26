2443668cf3fdecf9e438aa4f849446504f798eef
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-26 15:36:47 +0530

    Created

---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Exception/ClientNotFoundException.java
@@ -0,0 +1,7 @@
+package com.JobApplicationPortal.JobApplicationPortal.Exception;
+
+public class ClientNotFoundException extends RuntimeException {
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+}
