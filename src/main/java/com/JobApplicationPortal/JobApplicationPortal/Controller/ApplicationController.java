66f875717da9ba9e242f5903c24cb5bb9861cd39
siddhesh9201 <siddheshmali3050@gmail.com>
2025-07-29 15:19:27 +0530

    All EndPoints Are Defined

--- src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/ApplicationController.java
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/ApplicationController.java
@@ -1,4 +1,46 @@
 package com.JobApplicationPortal.JobApplicationPortal.Controller;
 
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.ApplicationMapper.IncomingApplicationDto;
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.ApplicationMapper.OutgoingApplication;
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.ApplicationMapper.OutgoingApplicationDto;
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.ApplicationMapper.OutgoingApplicationForRecruiter;
+import com.JobApplicationPortal.JobApplicationPortal.Services.ApplicationServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ApplicationController {
+
+
+    @Autowired
+    ApplicationServices applicationServices;
+
+
+    @PostMapping("/application/apply")
+    public ResponseEntity<String> addJobApplication(@RequestBody IncomingApplicationDto input){
+        return ResponseEntity.status(HttpStatus.CREATED).body(applicationServices.addApplication(input));
+    }
+
+    @GetMapping("/application/client/{clientId}")
+    public ResponseEntity<List<OutgoingApplicationDto>> getApplicationByClientId(@PathVariable Long clientId){
+        return ResponseEntity.status(HttpStatus.OK).body(applicationServices.getApplicationById(clientId));
+
+   }
+
+   @GetMapping("/application/job/{jobId}")
+   public ResponseEntity<List<OutgoingApplicationForRecruiter>> getApplicationByJobId(@PathVariable Long jobId){
+       return ResponseEntity.status(HttpStatus.OK).body(applicationServices.getApplicationByJobId(jobId));
+
+   }
+
+   @GetMapping("/application/{aId}")
+    public ResponseEntity<OutgoingApplication> getApplicationById(@PathVariable Long aId){
+         return ResponseEntity.status(HttpStatus.OK).body(applicationServices.getApplication(aId));
+   }
+
 }
---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Controller/NotificationController.java
@@ -0,0 +1,53 @@
+package com.JobApplicationPortal.JobApplicationPortal.Controller;
+
+
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.Notification.NotficationOutgoingDto;
+import com.JobApplicationPortal.JobApplicationPortal.Mapper.Notification.NotificationIncomingDto;
+import com.JobApplicationPortal.JobApplicationPortal.Services.NotificationServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class NotificationController {
+
+    @Autowired
+    NotificationServices notificationServices;
+
+    @GetMapping("/notification/allnotofications")
+    public ResponseEntity<Page<NotficationOutgoingDto>> getAllNotification(
+            @RequestParam(defaultValue = "0")int page,
+            @RequestParam(defaultValue = "5")int size,
+            @RequestParam(defaultValue = "asc")String direction,
+            @RequestParam(defaultValue = "message")String sortby)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(notificationServices.getAllNotifications(page,size,direction,sortby));
+    }
+
+    @PostMapping("/notification/selected")
+    public ResponseEntity<String> notify(@RequestBody NotificationIncomingDto notify){
+        return ResponseEntity.status(HttpStatus.CREATED).body(notificationServices.sendNotification(notify));
+    }
+    @PostMapping("/notification/Rejection")
+    public ResponseEntity<String> Rejection(@RequestBody NotificationIncomingDto notify){
+        return ResponseEntity.status(HttpStatus.CREATED).body(notificationServices.sendNotificationRejection(notify));
+    }
+    @DeleteMapping("notification/deletenotification/{id}")
+    public ResponseEntity<String> removeNotification(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(notificationServices.removeNotification(id));
+    }
+    @GetMapping("/notification/{clientId}")
+    public ResponseEntity<List<NotficationOutgoingDto>> getAllNotificationsByClientId(@PathVariable Long clientId) {
+        List<NotficationOutgoingDto> notifications = notificationServices.getAllByClientId(clientId);
+        return ResponseEntity.ok(notifications);
+    }
+    @PutMapping("/read/{notificationId}")
+    public ResponseEntity<String> markAsRead(@PathVariable Long notificationId) {
+        notificationServices.markNotificationAsRead(notificationId);
+        return ResponseEntity.ok("Notification marked as read.");
+    }
+}
