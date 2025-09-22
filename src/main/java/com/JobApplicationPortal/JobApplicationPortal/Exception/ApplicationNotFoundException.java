624105763ea7faec958cd31e75a08824084fca03
siddhesh9201 <siddheshmali3050@gmail.com>
2025-09-22 13:45:05 +0530

    some changes are made

---
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/Exception/ApplicationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.JobApplicationPortal.JobApplicationPortal.Exception;
+
+public class ApplicationNotFoundException extends RuntimeException {
+    public ApplicationNotFoundException(String message) {
+        super(message);
+    }
+}
