efe6f846eaa351067afa1c4ddb17443a9cf0dbb4
siddhesh9201 <siddheshmali3050@gmail.com>
2025-09-22 13:46:37 +0530

    Organized in packeage

--- src/main/java/com/JobApplicationPortal/JobApplicationPortal/EmailService/EmailService.java
+++ src/main/java/com/JobApplicationPortal/JobApplicationPortal/EmailService/EmailService.java
@@ -1,4 +1,4 @@
-package com.JobApplicationPortal.JobApplicationPortal;
+package com.JobApplicationPortal.JobApplicationPortal.EmailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
